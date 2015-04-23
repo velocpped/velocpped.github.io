fda333d0462def5f4931e4be3a412bd47c405800
MiF <grigorchuk.konstantin@gmail.com>
2015-04-23 16:35:12 +0300

    Добавлены странички и видео докладов

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,10 @@ module.exports = function(grunt) {
                     'speakers.html': ['templates/_header.html', 'templates/speakers.html', 'templates/_footer.html'],
                     'all.html': ['templates/_header.html', 'templates/all.html', 'templates/_footer.html'],
                     'place.html': ['templates/_header.html', 'templates/place.html', 'templates/_footer.html'],
-                    'man.html': ['templates/_header.html', 'templates/man.html', 'templates/_footer.html']
+                    'man.html': ['templates/_header.html', 'templates/man.html', 'templates/_footer.html'],
+                    '20150422-01-mergasov.html': ['templates/_header.html', 'templates/20150422/01_mergasov.html', 'templates/_footer.html'],
+                    '20150422-02-blohin.html': ['templates/_header.html', 'templates/20150422/02_blohin.html', 'templates/_footer.html'],
+                    '20150422-03-valiullin.html': ['templates/_header.html', 'templates/20150422/03_valiullin.html', 'templates/_footer.html']
                 }
             }
         }
